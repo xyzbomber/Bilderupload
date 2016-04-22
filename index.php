871cf1ad4cc0682b2dd2ce81d66b068de347fdbf
xyzbomber <brauckmann-jan@web.de>
2016-04-22 10:45:43 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<title>
+			[De-Clans] Bild-Upload
+		</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<center><h1>[De-Clans] Bild-Upload</h1></center>
+			</tr>
+			<tr>
+				<td>
+				&nbsp;&nbsp;
+				</td>
+				<td width="300px">
+					<center><h3>Navigation</h3></center>
+					<hr/>
+					<a href="#">Home</a><br />
+					<a href="bilder.php">Bilder</a><br />
+				</td>
+				<td width="30px">
+				</td>
+				<td width="100%">
+				<center>
+					<form action="upload.php" method="post" enctype="multipart/form-data">
+						<input type="file" name="datei"><br>
+						<input type="submit" value="Hochladen">
+					</form>
+				</center>
+				</td>
+			</tr>
+		</table>
+		<center>
+			&copy; 2015 - 2016 | <a href="https://de-clans.de/">[De-Clans] Community</a>
+		</center>
+	</body>
+</html>
